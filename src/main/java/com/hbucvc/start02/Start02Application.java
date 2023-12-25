ce6c32b274936ab3a391a35c93c60f9d03d758e4
dbliucc <2758445734@qq.com>
2023-12-25 20:41:22 +0800

    push test

---
+++ src/main/java/com/hbucvc/start02/Start02Application.java
@@ -0,0 +1,13 @@
+package com.hbucvc.start02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hbucvc/start02/Start02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbucvc.start02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Start02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
